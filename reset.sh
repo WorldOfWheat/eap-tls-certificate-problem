610f8ac8cf8d7547a4546fdc412cc47b3710b132
qilin102223 <qilin102223@gmail.com>
2025-08-01 00:24:34 +0800

    fix: mv eap and change Dockerfile in freeradius

--- reset.sh
+++ reset.sh
@@ -1,2 +1,2 @@
-docker compose kill && docker compose rm -f
-docker compose up -d --force-recreate --build
+docker-compose kill && docker-compose rm -f
+docker-compose up -d --force-recreate --build
